b71a772c564cb9dc1a274487ded5eb1bf72b1f54
dimkijeee <dimkijeee@ukr.net>
2018-07-20 17:06:23 +0300

    Playing with formatting

--- WebAPI_Project/WebAPI_Project/Controllers/ValuesController.cs
+++ WebAPI_Project/WebAPI_Project/Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace WebAPI_Project.Controllers
     {
         // GET: api/<controller>
         [HttpGet]
+        [Produces("application/json")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
--- WebAPI_Project/WebAPI_Project/Startup.cs
+++ WebAPI_Project/WebAPI_Project/Startup.cs
@@ -15,7 +15,8 @@ namespace WebAPI_Project
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
